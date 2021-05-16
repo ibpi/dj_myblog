65e6b967f94e88c302f78e251914819a7e8c711a
WJK <1045579531@qq.com>
2021-05-16 16:04:15 +0800

    add hot_data_read_num

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render_to_response
 from django.contrib.contenttypes.models import ContentType
-from read_statistics.utils import get_seven_days_read_data
+from read_statistics.utils import (
+    get_seven_days_read_data,
+    get_today_hot_data,
+    get_yesterday_hot_data,
+    get_7_days_hot_data
+)
 from blog.models import Blog
 
 
@@ -11,4 +16,7 @@ def home(request):
     context = {}
     context["dates"] = dates
     context["read_nums"] = read_nums
+    context["today_hot_data"] = get_today_hot_data(blog_content_type)
+    context["yesterday_hot_data"] = get_yesterday_hot_data(blog_content_type)
+    context["hot_data_for_7_days"] = get_7_days_hot_data(blog_content_type)
     return render_to_response("home.html", context)
