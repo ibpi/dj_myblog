669a15b33e8e4580a6cce25cf416aa86da622bae
WJK <1045579531@qq.com>
2021-05-22 14:10:19 +0800

    add reply

--- blog/views.py
+++ blog/views.py
@@ -84,14 +84,15 @@ def blog_detail(request, blog_pk):
     blog = get_object_or_404(Blog, pk=blog_pk)
     read_cookie_key = read_statistics_once_read(request, blog)
     blog_content_type = ContentType.objects.get_for_model(blog)
-    comments = Comment.objects.filter(content_type=blog_content_type, object_id=blog.pk)
+    comments = Comment.objects.filter(content_type=blog_content_type, object_id=blog.pk, parent=None)
 
     context = {}
     context["blog"] = blog
-    context["comments"] = comments
+    context["comments"] = comments.order_by('-comment_time')
     data = {}
     data["content_type"] = blog_content_type.model
     data["object_id"] = blog_pk
+    data['reply_comment_id'] = 0
     context["comment_form"] = CommentForm(initial=data)
     context["next_blog"] = Blog.objects.filter(
         created_time__gt=blog.created_time
